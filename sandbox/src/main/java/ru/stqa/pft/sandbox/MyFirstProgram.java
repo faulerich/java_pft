971541f581465b419429fa971cca0aebd6446690
Evgeny Bondarenko <faulerich@yandex.ru>
2017-05-15 02:06:18 +0300

    Для вычисления площадей фигур использованы методы и классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,22 +5,15 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello();
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
+
   public static void hello() {
     System.out.println("Hello, world!");
   }
 
-  public static double area (double len) {
-    return len * len;
-  }
-
-  public static double area (double a, double b) {
-    return a * b;
-  }
 }
