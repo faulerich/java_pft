12bea4f8d489f7a6b13c7ceb2cfd361fc6764bab
Evgeny Bondarenko <faulerich@yandex.ru>
2017-05-15 18:38:10 +0300

    Функционал вычисления расстояния между двумя точками на плоскости

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,17 +3,21 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello();
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    //проверка работоспособности функции вычисления расстояния distance
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(6, 7);
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Расстояние между точками с координатами (" + p1.x + " ; " + p1.y + ") и (" + p2.x + " ; " + p2.y + ") равно " + distance(p1, p2));
+
+
+    //проверка работоспособности метода, описанного в классе Point
+    System.out.println("Расстояние между точками с координатами (" + p1.x + " ; " + p1.y + ") и (" + p2.x + " ; " + p2.y + ") равно " + Point.distance(p1, p2));
   }
 
-  public static void hello() {
-    System.out.println("Hello, world!");
+  public static double distance(Point p1, Point p2) {
+
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
   }
 
 }
