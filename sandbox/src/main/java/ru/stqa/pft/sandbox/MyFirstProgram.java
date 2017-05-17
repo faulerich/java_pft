1237577d1c78e30311a4360fe122b58d0d492706
Evgeny Bondarenko <faulerich@yandex.ru>
2017-05-17 13:46:19 +0300

    Вернул в главный класс вычисление площадей фигур

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,13 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
     //проверка работоспособности функции вычисления расстояния distance
     Point p1 = new Point(2, 4);
     Point p2 = new Point(6, 7);
