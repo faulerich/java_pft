7e53c44f268c96d40281ef85d47a95cdc2401d51
Evgeny Bondarenko <faulerich@yandex.ru>
2017-05-18 11:24:11 +0300

    Изменен текст сообщения

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -12,18 +12,18 @@ public class DistanceTests {
   public void testDistance() {
     Point p1 = new Point(2, 4);
     Point p2 = new Point(6, 7);
-    Assert.assertEquals(p1.distance(p2), 5.0, "Ошибка!");
+    Assert.assertEquals(p1.distance(p2), 5.0, "Ошибка!!!!");
 
     Point p3 = new Point(0, 0);
     Point p4 = new Point(0, -1);
-    Assert.assertEquals(p3.distance(p4), 1.0, "Ошибка!");
+    Assert.assertEquals(p3.distance(p4), 1.0, "Ошибка!!!!");
 
     Point p5 = new Point(1, 1);
     Point p6 = new Point(-1, -1);
-    Assert.assertEquals(p5.distance(p6), Math.sqrt(8), "Ошибка!");
+    Assert.assertEquals(p5.distance(p6), Math.sqrt(8), "Ошибка!!!!");
 
     Point p7 = new Point(0, 0);
     Point p8 = new Point(0, 0);
-    Assert.assertEquals(p7.distance(p8), 0.0, "Ошибка!");
+    Assert.assertEquals(p7.distance(p8), 0.0, "Ошибка!!!!");
   }
 }
