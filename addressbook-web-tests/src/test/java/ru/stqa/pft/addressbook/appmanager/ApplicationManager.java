36ed13e4dd3d877e1f56603fe0cd7f5641ee4b08
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-05-29 11:27:11 +0300

    Поменял импорты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,17 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import sun.plugin2.util.BrowserType;
+//import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
+
 
 import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.remote.BrowserType.IE;
 
 /**
  * Created by Bond on 21.05.2017.
@@ -24,18 +23,18 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
-  private int browser;
+  private String browser;
 
-  public ApplicationManager(int browser) {
+  public ApplicationManager(String browser) {
     this.browser = browser;
   }
 
   public void init() {
-    if (browser == BrowserType.MOZILLA) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser == BrowserType.DEFAULT) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.INTERNET_EXPLORER) {
+    } else if (browser.equals(BrowserType.IE)) {
       DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
       ieCapabilities.setCapability(
               InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,14 +3,15 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import sun.plugin2.util.BrowserType;
+//import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 /**
  * Created by Bond on 20.05.2017.
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.MOZILLA);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod
   public void setUp() throws Exception {
