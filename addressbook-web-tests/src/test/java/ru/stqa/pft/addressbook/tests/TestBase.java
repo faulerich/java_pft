708ada73cf2099427c48ad347de0212a734896b1
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-06-08 12:53:04 +0300

    Однократный запуск браузера для прогонки всех тестов
    Лекция 5.1
    Используется BeforeSuite вместо BeforeMethod
    Сделали переменную app статической, в результате чего к ней можно
    обращаться из всех классов.
    Браузер открывается один раз перед стартом всех тестов и закрывается
    один раз после окончания всех тестов.
    Время выполнения тестов уменьшилось почти в 2 раза.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,25 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-//import sun.plugin2.util.BrowserType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+//import sun.plugin2.util.BrowserType;
 
 /**
  * Created by Bond on 20.05.2017.
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
-
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
