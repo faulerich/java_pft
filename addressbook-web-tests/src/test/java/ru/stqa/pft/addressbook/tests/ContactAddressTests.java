cf3cb53ae6c280ac62da74bdd0a51cfdeb8b0044
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-06-20 12:28:12 +0300

    небольшие дополнения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -10,6 +11,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by Bond on 14.06.2017.
  */
 public class ContactAddressTests extends TestBase {
+
+  @BeforeMethod   //перед каждым тестовым методом должна выполняться проверка предусловия
+  public void ensurePreconditions() {  //проверяем предусловия: если список контактов пуст, то создаем контакт
+    app.goTo().contactList();
+    if (app.contact().list().size() == 0) {
+      app.contact().create(new ContactData().withFirstName("Yevgeny").withLastName( "Bondarenko").withAddress("VRN").withHomephone("123").withEmail("test@test.com").withBirthyear("1985").withGroup("[none]"), true);
+    }
+  }
 
   @Test
   public void testContactAddress() {
