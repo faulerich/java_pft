96256b2ee3b43077513232007f3c635d49f87b38
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-07-01 18:10:03 +0300

    Тест удаления контакта из группы
    д/з №16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContactFromGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContactFromGroup.java
@@ -35,17 +35,25 @@ public class DeleteContactFromGroup extends TestBase {
 
   public void testDeleteContactFromGroup() {
 
+    //если нет группы с контактами
+    if (app.db().situatedGroupForRemoveContact() == null) {
+      app.goTo().contactList();
+      app.contact().create(new ContactData()
+              .withFirstName("Yevgeny").withLastName("Bondarenko_new").withHomephone("123").withEmail("test@test.com").withBirthyear("1985").withAddress("qqq")
+              .inGroup(app.db().groups().iterator().next()), true);
+    }
+
   Contacts beforeContacts = app.db().situatedGroupForRemoveContact().getContacts(); //список контактов в подобранной группе на данный момент
     int situatedGroupIDForRemoveContact = app.db().situatedGroupForRemoveContact().getId();
-    System.out.println("в подобранной группе id " + app.db().situatedGroupForRemoveContact().getId() + " контакты: " + beforeContacts);
+
     app.goTo().contactList();
     app.contact().contactsFilterByGroup(situatedGroupIDForRemoveContact);
     ContactData selectedContact = beforeContacts.iterator().next(); //контакт для препарирования
     app.contact().selectElementById(selectedContact.getId());
     app.contact().deleteContactFromGroupButton();
     app.goTo().contactList();
-    Contacts afterContacts = app.db().situatedGroupForRemoveContact().getContacts(); //список контактов в подобранной группе на данный момент (после удаление из нее контакта)
-    System.out.println("в подобранной группе id " + app.db().situatedGroupForRemoveContact().getId() + " контакты: " + afterContacts);
+
+    Contacts afterContacts = app.db().getGroupFromDb(situatedGroupIDForRemoveContact).getContacts(); //список контактов в подобранной группе на данный момент (после удаления из нее контакта)
 
     assertThat(afterContacts, equalTo(beforeContacts.withoutAdded(selectedContact)));
 }
