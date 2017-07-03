d8dcd3e9e607bc6af461be9259b5ceaa53af7e70
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-07-03 09:15:08 +0300

    Добавлен новый модуль для тестов для Mantis
    Задание №17

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,63 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+//import sun.plugin2.util.BrowserType;
+
+/**
+ * Created by Bond on 21.05.2017.
+ */
+public class ApplicationManager {
+
+  private final Properties properties;
+  WebDriver wd;
+
+  private String browser;
+
+
+  public ApplicationManager(String browser) throws IOException {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+      ieCapabilities.setCapability(
+              InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+              true
+      );
+      wd = new InternetExplorerDriver(ieCapabilities);
+    }
+
+
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+
+  }
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+
+/**
+ * Created by Bond on 20.05.2017.
+ */
+public class TestBase {
+
+  protected static ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
