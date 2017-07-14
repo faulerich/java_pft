e8b0fb2a3198fe8d0f39de836a1b51fa41b73672
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-07-15 01:43:15 +0300

    убран неиспользуемый импорт

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
