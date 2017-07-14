29f07c92f7e17ebc7a1c27fac06847b715b83c39
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-07-15 01:07:10 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,10 +9,13 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -21,69 +26,77 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-  private final Properties properties;
-  WebDriver wd;
+    private final Properties properties;
+    WebDriver wd;
 
-  private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private ContactHelper contactHelper;
-  private String browser;
-  private DbHelper dbHelper;
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private GroupHelper groupHelper;
+    private ContactHelper contactHelper;
+    private String browser;
+    private DbHelper dbHelper;
 
-  public ApplicationManager(String browser) throws IOException {
-    this.browser = browser;
-    properties = new Properties();
-  }
+    public ApplicationManager(String browser) throws IOException {
+        this.browser = browser;
+        properties = new Properties();
+    }
 
-  public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    dbHelper = new DbHelper();
+        dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-      ieCapabilities.setCapability(
-              InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
-              true
-      );
-      wd = new InternetExplorerDriver(ieCapabilities);
+        //если свойство selenium server - пустая строка (используется local.properties), то инициализация - как обычно
+        if ("".equals(properties.getProperty("selenium.server"))) {
+
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+                ieCapabilities.setCapability(
+                        InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+                        true
+                );
+                wd = new InternetExplorerDriver(ieCapabilities);
+            } else //если свойство selenium server - непустая строка (используется selenium server), то используем другой тип драйвера
+            {
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName(browser);
+                wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+            }
+        }
+
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+        groupHelper = new GroupHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
 
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-  }
+    public void stop() {
+        wd.quit();
+    }
 
 
-  public void stop() {
-    wd.quit();
-  }
+    public GroupHelper group() {
+        return groupHelper;
+    }
 
+    public NavigationHelper goTo() {
+        return navigationHelper;
+    }
 
-  public GroupHelper group() {
-    return groupHelper;
-  }
+    public ContactHelper contact() {
+        return contactHelper;
+    }
 
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
-
-  public ContactHelper contact() {
-    return contactHelper;
-  }
-
-  public DbHelper db() {
-    return dbHelper;
-  }
+    public DbHelper db() {
+        return dbHelper;
+    }
 }
