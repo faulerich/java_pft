5987bae37dbb39e782cecf475aa9ea522346c356
Evgeny Bondarenko <yevgeny_bondarenko@relex.ru>
2017-07-17 01:22:47 +0300

    р

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -67,7 +67,7 @@ public class ApplicationManager {
             }
         }
 
-        //wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
